38d8de7ba3307762c7d54ae3626b6bc7eb0837b5
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-11-15 06:59:00 +0300

    Add a test on errors

---
+++ tests/Main.hs
@@ -0,0 +1,30 @@
+module Main
+where
+
+import Prelude hiding (assert, choose, (.&.), Success, Failure, Fixed)
+import Bug
+import Test.Tasty
+import Test.Tasty.Runners
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck hiding (Success, Failure)
+import Test.QuickCheck.Instances
+import qualified Belka.IO as A
+import qualified Belka.Interact as B
+import qualified Belka.Request as C
+import qualified Belka.ParseBody as D
+
+
+main =
+  defaultMain $
+  testGroup "All tests" $
+  [
+    testCase "Erroneous response" $ do
+      result <-
+        fmap
+          (either Left (either (Left . fromString . show) Right))
+          (A.interactUsingGlobalManager
+            (B.request
+              (C.setUrlUnsafe "http://localhost:993")
+              (pure (fmap (const ()) D.json))))
+      assertBool "" (isLeft result)
+  ]
