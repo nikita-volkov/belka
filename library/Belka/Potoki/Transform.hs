26c14fe6369c14411ed225605358bc2d342c83ee
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-12-01 11:55:22 +0300

    Change the Manager stuff

--- library/Belka/Potoki/Transform.hs
+++ library/Belka/Potoki/Transform.hs
@@ -14,28 +14,40 @@ import qualified Belka.Interact as I
 
 
 {-|
+Given a specification of how to parse the response,
+execute the requests, producing the parsed responses.
+-}
+request :: C.ParseHead (D.ParseBody response) -> Transform B.Request (Either Text (Either G.TransportError response))
+request parse =
+  arr (\ request -> A.newTlsManager >>= \ manager -> H.interact manager (I.request request parse) <* A.closeManager manager) >>>
+  executeIO
+
+{-|
 Given an HTTP manager, a specification of how to parse the response,
 execute the requests, producing the parsed responses.
 -}
-request :: A.Manager -> C.ParseHead (D.ParseBody response) -> Transform B.Request (Either Text (Either G.TransportError response))
-request manager parse =
+requestUsingManager :: A.Manager -> C.ParseHead (D.ParseBody response) -> Transform B.Request (Either Text (Either G.TransportError response))
+requestUsingManager manager parse =
   arr (\ request -> H.interact manager (I.request request parse)) >>>
   executeIO
 
 {-|
-Perform 'request' using a new manager.
+Perform 'requestUsingManager' using a new manager.
 -}
 requestUsingNewManager :: C.ParseHead (D.ParseBody response) -> Transform B.Request (Either Text (Either G.TransportError response))
 requestUsingNewManager parse =
   ioTransform $ do
-    manager <- A.newTlsManager
-    return (request manager parse)
+    manager <- A.newTlsManagerWith settings
+    return (requestUsingManager manager parse)
+  where
+    settings =
+      A.tlsManagerSettings
 
 {-|
-Perform 'request' using a global manager.
+Perform 'requestUsingManager' using a global manager.
 -}
 requestUsingGlobalManager :: C.ParseHead (D.ParseBody response) -> Transform B.Request (Either Text (Either G.TransportError response))
 requestUsingGlobalManager parse =
   ioTransform $ do
     manager <- A.getGlobalManager
-    return (request manager parse)
+    return (requestUsingManager manager parse)
